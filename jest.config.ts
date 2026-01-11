3db185a7a25225e96ab037481b2be987d33e6c20
simranmiglani <97simran.miglani@gmail.com>
2026-01-11 10:13:38 -0800

    Setup: Missing TypeScript test setup - Add jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/__tests__'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: { global: { lines: 0 } }
+};
